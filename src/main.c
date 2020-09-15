6c96b097e574efb37ddd8be1498244ed67c4301e
landinij0040 <ijlandin@miners.utep.edu>
2020-09-15 09:45:17 -0600

    Error In Main Space Char Function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include "tokenizer.h"
+#include "history.h"
+
+#define MAXWORD 100
+/* true if c is a tab or space, and not zero */
+int space_char(char c)
+{
+  if(c == "\t" || c == " ")
+    return 1;
+  else
+    return 0;
+
+
+
+}
 
 int main()
 {
-  printf("Hello World");
-
-
+  /* To hold value from user */
+  char s[MAXWORD];
+  printf(">");
+  scanf("%[^\n]", &s);
+  printf("%s\n",s);
   return 0;
 }
+
--- src/tester.c
+++ src/tester.c
@@ -96,15 +96,15 @@ static char *all_tests() {
     return 0;
 }
 
- int main(int argc, char **argv) {   
-    char *result = all_tests();
+//int main(int argc, char **argv) {   
+//    char *result = all_tests();
 
-    if (result != 0) 
-        printf("Failed test: %s\n", result);
-    else 
-        printf("ALL TESTS PASSED\n");
+//    if (result != 0) 
+//        printf("Failed test: %s\n", result);
+//    else 
+//        printf("ALL TESTS PASSED\n");
     
-    printf("Tests run: %d\n", tests_run);
+//    printf("Tests run: %d\n", tests_run);
 
-    return result != 0;
+//    return result != 0;
  }
